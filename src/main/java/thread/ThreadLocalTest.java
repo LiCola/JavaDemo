6db18b41c6f1c8f3a545c828474a78422cc34f72
李可乐 <361401376@qq.com>
2018-05-28 16:46:15 +0800

    调试线程池分析其中原理

--- src/main/java/thread/ThreadLocalTest.java
+++ src/main/java/thread/ThreadLocalTest.java
@@ -1,29 +1,47 @@
 package thread; import com.licola.llogger.LLogger;
+import java.util.Random;
 
 /**
  * Created by LiCola on 2017/10/13.
  */
 public class ThreadLocalTest {
 
-  static ThreadLocal<Integer> local = new ThreadLocal<>();
+   static ThreadLocal<Integer> localInt = new ThreadLocal<Integer>(){
+    @Override
+    protected Integer initialValue() {
+      LLogger.d("initialValue 有机会给get null 的情况 返回初始值");
+      return super.initialValue();
+    }
+  };
+
+   static ThreadLocal<String> localStr=new ThreadLocal<>();
 
   public static final void main(String[] args) throws InterruptedException {
+    threadsGetAndSet();
+
+    Random random=new Random();
+//    ThreadLocalRandom threadLocalRandom=ThreadLocalRandom.current();
+  }
+
+  private static void threadsGetAndSet() throws InterruptedException {
     Thread childThread = new Thread(new Runnable() {
       @Override
       public void run() {
-        LLogger.d("child thread local init value:" + local.get());
-        local.set(200);
-        LLogger.d("child thread local value:" + local.get());
+        LLogger.d("child thread localInt init value:" + localInt.get());
+        localStr.set("子线程 也拥有自己的本地变量 而且是多个");
+        localInt.set(200);
+        LLogger.d("child thread localInt value:" + localInt.get());
+        LLogger.d("child thread localStr value:" + localStr.get());
       }
     });
 
-    local.set(100);
+    localStr.set("每个线程都能和ThreadLocal线程本地变量关联起来，而且一个线程可以关联多个ThreadLocal");
+    localInt.set(100);
     childThread.start();
     childThread.join();
 
-    LLogger.d("main thread local:" + local.get());
-
-//    ThreadLocalRandom threadLocalRandom=ThreadLocalRandom.current();
+    LLogger.d("main thread localInt:" + localInt.get());
+    LLogger.d("main thread localStr:"+localStr.get());
   }
 
 
--- src/main/java/thread/ThreadPoolTest.java
+++ src/main/java/thread/ThreadPoolTest.java
@@ -1,4 +1,6 @@
-package thread; import com.licola.llogger.LLogger;
+package thread;
+
+import com.licola.llogger.LLogger;
 
 import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
@@ -9,6 +11,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -18,45 +21,83 @@ import java.util.concurrent.TimeUnit;
 public class ThreadPoolTest {
 
   private static final int THREAD_SIZE = 5;
-//  static ExecutorService executorService = Executors.newFixedThreadPool(THREAD_SIZE);//任务数>=线程数时 发生死锁
+  //  static ExecutorService executorService = Executors.newFixedThreadPool(THREAD_SIZE);//任务数>=线程数时 发生死锁
 //  static ExecutorService executorService = Executors.newCachedThreadPool();//不限定线程数 不会死锁
-  static ExecutorService executorService = new ThreadPoolExecutor(THREAD_SIZE,THREAD_SIZE<<1,10,
-    TimeUnit.SECONDS,new LinkedBlockingDeque<>());
-
+  static ExecutorService executorService = new ThreadPoolExecutor(THREAD_SIZE, THREAD_SIZE << 1, 10,
+      TimeUnit.SECONDS, new LinkedBlockingDeque<>());
 
 
   public static final void main(String[] args) throws InterruptedException {
-    threadPoolDeadlock();
+//    threadPoolDeadlock(Executors.newSingleThreadExecutor());
 
-    
+    ThreadPoolExecutor service = (ThreadPoolExecutor) Executors.newCachedThreadPool(new ThreadFactory() {
+      @Override
+      public Thread newThread(Runnable r) {
+        LLogger.d("线程创建");
+        Thread thread = new Thread(r);
+        return thread;
+      }
+    });
+    service.execute(new Runnable() {
+      @Override
+      public void run() {
+//        LLogger.trace();
+        LLogger.d("run 1");
+      }
+    });
+
+    Thread.sleep(100);
+
+    service.execute(new Runnable() {
+      @Override
+      public void run() {
+//        LLogger.trace();
+        LLogger.d("run 2");
+      }
+    });
+//    service.execute(new Runnable() {
+//      @Override
+//      public void run() {
+//        LLogger.d("run 3");
+//      }
+//    });
+//    service.execute(new Runnable() {
+//      @Override
+//      public void run() {
+////        LLogger.trace();
+//        LLogger.d("run 3");
+//      }
+//    });
   }
 
 
   private static class TaskA implements Runnable {
 
     private final int taskId;
+    private final ExecutorService executorService;
 
-    public TaskA(int taskId) {
+    public TaskA(int taskId, ExecutorService executorService) {
       this.taskId = taskId;
+      this.executorService = executorService;
     }
 
     @Override
     public void run() {
-      LLogger.d(Thread.currentThread() +" task id:"+taskId+" run");
+      LLogger.d(" task id:" + taskId + " run");
       try {
-        Thread.sleep((long) (1000*Math.random()));
+        Thread.sleep((long) (1000 * Math.random()));
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
 
-//      Future<?> submit = executorService.submit(new TaskB());
+      Future<?> submit = executorService.submit(new TaskB());
 //
-//      try {
-//        submit.get();
-//      } catch (InterruptedException | ExecutionException e) {
-//        e.printStackTrace();
-//      }
-      LLogger.d(Thread.currentThread() +" task id:"+taskId+" finish");
+      try {
+        submit.get();
+      } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+      }
+      LLogger.d(" task id:" + taskId + " finish");
     }
   }
 
@@ -64,16 +105,17 @@ public class ThreadPoolTest {
 
     @Override
     public void run() {
-      LLogger.d(Thread.currentThread() + " task B execute and finish");
+      LLogger.d(" task B execute and finish");
     }
   }
 
-  private static void threadPoolDeadlock() throws InterruptedException {
-    int workNumber=30;
+  private static void threadPoolDeadlock(ExecutorService executorService)
+      throws InterruptedException {
+    int workNumber = 2;
     for (int i = 0; i < workNumber; i++) {
-      executorService.execute(new TaskA(i));
+      executorService.execute(new TaskA(i, executorService));
     }
-    executorService.shutdown();
+//    executorService.shutdown();
   }
 
 
